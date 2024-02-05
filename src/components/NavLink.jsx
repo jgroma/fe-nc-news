a5656a9e2e8df503dbb87b6c30b0d881d5941307
jgroma <jgromadzinska9@gmail.com>
2024-02-05 15:56:33 +0000

    Complete AllArticles route

---
+++ src/components/NavLink.jsx
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom";
+export default function NavLink({ linkDestination, linkName }) {
+  return (
+    <li>
+      <Link to={`/${linkDestination}`}>{linkName}</Link>
+    </li>
+  );
+}
